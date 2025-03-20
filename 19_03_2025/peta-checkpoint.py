9153f0d2bf944086f1c72c583e0309f8f5fe7d0d
Nuria Rodriguez Pardo <nuria.rodriguez.pardo@estudiantat.upc.edu>
2025-03-20 16:06:49 +0100

    Entrega

---
+++ 19_03_2025/peta-checkpoint.py
@@ -0,0 +1,12 @@
+# from divide import divide
+
+exec(open('divide.py').read())
+
+def peta(x, y):
+    return peta2(x, y)
+
+def peta2(x, y):
+    return peta3(x, y)
+
+def peta3(x, y):
+    return divide(x, y)
